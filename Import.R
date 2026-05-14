ab972360047302eec09671cd6559913c838c170e
Péter Kalicz <kaliczp@gmail.com>
2026-05-14 17:23:35 +0200

    Correct reorder

--- Import.R
+++ Import.R
@@ -1,6 +1,6 @@
 library(readxl)
 Rangsor <- read_excel("kalicz_peti_CEE_abrak.xlsx")
 names(Rangsor) <- c("Country", "Index")
-RangsorO <- Rangsor[order(Rangsor$Index, decreasing = TRUE),]
-factor(RangsorO$Country, levels = RangsorO$Country)
+RangsorO <- Rangsor[order(Rangsor$Index),]
+RangsorO$Country <- factor(RangsorO$Country, levels = RangsorO$Country)
 makeRangsor(RangsorO)
