895c68ec6e3e56a3a5cb25a88e910744f34cad1e
Péter Kalicz <kaliczp@gmail.com>
2026-05-14 16:59:07 +0200

    Factor and no label

--- Import.R
+++ Import.R
@@ -1,4 +1,6 @@
 library(readxl)
 Rangsor <- read_excel("kalicz_peti_CEE_abrak.xlsx")
 names(Rangsor) <- c("Country", "Index")
-makeRangsor(Rangsor)
+RangsorO <- Rangsor[order(Rangsor$Index, decreasing = TRUE),]
+factor(RangsorO$Country, levels = RangsorO$Country)
+makeRangsor(RangsorO)
--- Rangsor.R
+++ Rangsor.R
@@ -2,6 +2,12 @@ makeRangsor <- function(x) {
     require(ggplot2)
     out <- ggplot(data = x, aes(x=Index, y=Country)) +
         geom_segment(aes(yend=Country, xend=0), colour= "darkgreen")+
-        geom_vline(xintercept=0, colour = "darkgrey")
+        geom_vline(xintercept=0, colour = "darkgrey") +
+        theme_classic() +
+        labs(x = "Index [%]", y = NULL) +
+        geom_text(aes(label = round(Index,1)), colour = "black", size = 2.9)+
+        scale_y_discrete(labels = x$Country) +
+    guides(y = "none")
+
     out
 }
