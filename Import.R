2ee6fe85a990bb52a071a03019bc2f43305e3ae3
Péter Kalicz <kaliczp@gmail.com>
2026-05-14 17:51:40 +0200

    Labels corrected
    Labels plotted, limits are set explicitly

--- Import.R
+++ Import.R
@@ -3,4 +3,4 @@ Rangsor <- read_excel("kalicz_peti_CEE_abrak.xlsx")
 names(Rangsor) <- c("Country", "Index")
 RangsorO <- Rangsor[order(Rangsor$Index),]
 RangsorO$Country <- factor(RangsorO$Country, levels = RangsorO$Country)
-makeRangsor(RangsorO)
+makeRangsor(RangsorO, xlim = c(-6,33))
--- Rangsor.R
+++ Rangsor.R
@@ -1,13 +1,17 @@
-makeRangsor <- function(x) {
+makeRangsor <- function(x, xlim = NULL) {
     require(ggplot2)
+    x$Place <- -0.5
     out <- ggplot(data = x, aes(x=Index, y=Country)) +
         geom_segment(aes(yend=Country, xend=0), colour= "darkgreen")+
         geom_vline(xintercept=0, colour = "darkgrey") +
         theme_classic() +
         labs(x = "Index [%]", y = NULL) +
-        geom_text(aes(label = round(Index,1)), colour = "black", size = 2.9)+
-        scale_y_discrete(labels = x$Country) +
-    guides(y = "none")
-
+        geom_text(aes(label = round(Index,1)), colour = "black", size = 2.9) +
+        geom_text(aes(x = Place, label = Country, hjust = "right"),
+                  colour = "darkgreen") +
+        guides(y = "none")
+    if(!is.null(xlim)) {
+        out <- out + scale_x_continuous(limits = xlim)
+    }
     out
 }
