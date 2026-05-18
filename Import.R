764266ee64e883c639fe0681fb2d7002cfe5af4b
Péter Kalicz <kaliczp@gmail.com>
2026-05-18 14:11:04 +0200

    2nd sheet import prepared

--- Import.R
+++ Import.R
@@ -1,4 +1,5 @@
 library(readxl)
+## First table heat index
 Rangsor <- read_excel("kalicz_peti_CEE_abrak.xlsx")
 names(Rangsor) <- c("Country", "Index")
 ## Abbreviated names
@@ -6,3 +7,6 @@ Rangsor[c(7, 23, 24, 36, 39),"Country"] <- c("Bosnia", "Netherl.", "N. Maced.", 
 RangsorO <- Rangsor[order(Rangsor$Index),]
 RangsorO$Country <- factor(RangsorO$Country, levels = RangsorO$Country)
 makeRangsor(RangsorO, xlim = c(-6,33))
+
+## Second table
+T1SubsectRegion <- read_excel("kalicz_peti_CEE_abrak.xlsx", sheet = 2)
