f82f91cb9727f239cc7aed38d4c02fe02bae3d77
Péter Kalicz <kaliczp@gmail.com>
2026-05-18 16:35:56 +0200

    Initial database organisation

--- Import.R
+++ Import.R
@@ -9,4 +9,14 @@ RangsorO$Country <- factor(RangsorO$Country, levels = RangsorO$Country)
 makeRangsor(RangsorO, xlim = c(-6,33))
 
 ## Second table
-T1SubsectRegion <- read_excel("kalicz_peti_CEE_abrak.xlsx", sheet = 2)
+T1SubsectRegion.Type <- as.character(read_excel("kalicz_peti_CEE_abrak.xlsx", sheet = 2, col_names = FALSE, range = 'B2:L2'))
+T1SubsectRegion.Type <- T1SubsectRegion.Type[!T1SubsectRegion.Type == "NA"]
+T1SubsectRegion.Date <- as.character(read_excel("kalicz_peti_CEE_abrak.xlsx", sheet = 2, col_names = FALSE, range = 'B3:M3'))
+T1SubsectRegion.Region <- as.character(t(read_excel("kalicz_peti_CEE_abrak.xlsx", sheet = 2, col_names = FALSE, range = 'A5:A11')))
+T1SubsectRegion <- read_excel("kalicz_peti_CEE_abrak.xlsx", sheet = 2, col_names = FALSE, range = 'B5:M11')
+
+data.frame(Region = rep(T1SubsectRegion.Region),
+           Type = rep(T1SubsectRegion.Type, each = 14),
+           Date = rep(T1SubsectRegion.Date, each = 7),
+           Value = as.numeric(as.matrix(T1SubsectRegion))
+           )
