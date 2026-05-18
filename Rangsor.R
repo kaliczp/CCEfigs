6ebf9c4d80b39fc28e48d265ec3d9aada5236910
Péter Kalicz <kaliczp@gmail.com>
2026-05-18 10:31:49 +0200

    Central colour settings

--- Rangsor.R
+++ Rangsor.R
@@ -1,14 +1,15 @@
 makeRangsor <- function(x, xlim = NULL) {
+    basecolour <- "darkgreen"
     require(ggplot2)
     x$Place <- -0.5
     out <- ggplot(data = x, aes(x=Index, y=Country)) +
-        geom_segment(aes(yend=Country, xend=0), colour= "darkgreen")+
+        geom_segment(aes(yend=Country, xend=0), colour= basecolour)+
         geom_vline(xintercept=0, colour = "darkgrey") +
         theme_classic() +
         labs(x = "Index [%]", y = NULL) +
         geom_text(aes(label = round(Index,1)), colour = "black", size = 2.9) +
         geom_text(aes(x = Place, label = Country, hjust = "right"),
-                  colour = "darkgreen") +
+                               colour = basecolour) +
         guides(y = "none")
     if(!is.null(xlim)) {
         out <- out + scale_x_continuous(limits = xlim)
