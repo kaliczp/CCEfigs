a9d7db7d8f2ec04c5b93e02bfa2d6f4624808658
Péter Kalicz <kaliczp@gmail.com>
2026-05-18 11:28:06 +0200

    Abbreviated country names

--- Import.R
+++ Import.R
@@ -1,6 +1,8 @@
 library(readxl)
 Rangsor <- read_excel("kalicz_peti_CEE_abrak.xlsx")
 names(Rangsor) <- c("Country", "Index")
+## Abbreviated names
+Rangsor[c(7, 23, 24, 36, 39),"Country"] <- c("Bosnia", "Netherl.", "N. Maced.", "Switzerl.", "U. K.")
 RangsorO <- Rangsor[order(Rangsor$Index),]
 RangsorO$Country <- factor(RangsorO$Country, levels = RangsorO$Country)
 makeRangsor(RangsorO, xlim = c(-6,33))
