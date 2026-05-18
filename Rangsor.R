daf82d2dbe9c1b5b536dd7b919ef94f0685c416c
Péter Kalicz <kaliczp@gmail.com>
2026-05-18 10:50:53 +0200

    Uniform boxwidth
    Trailing zeroes are printed in the box and text too.

--- Rangsor.R
+++ Rangsor.R
@@ -1,6 +1,7 @@
 makeRangsor <- function(x, xlim = NULL, shape = "rectangle") {
     basecolour <- "darkgreen"
     require(ggplot2)
+    x$PlotData <- sprintf("%.1f", round(x$Index, digits = 1))
     x$Place <- -0.5
     out <- ggplot(data = x, aes(x=Index, y=Country)) +
         geom_segment(aes(yend=Country, xend=0), colour= basecolour)+
@@ -11,10 +12,11 @@ makeRangsor <- function(x, xlim = NULL, shape = "rectangle") {
                                colour = basecolour) +
         guides(y = "none")
     if(shape == "rectangle") {
-        out <- out + geom_label(aes(label = round(Index,1)),
+        out <- out + geom_label(aes(label = PlotData),
                                 colour = "white" , size = 2.9, fill = basecolour)
     } else {
-        out <- out + geom_text(aes(label = round(Index,1)), colour = "black", size = 2.9)
+        out <- out + geom_text(aes(label = PlotData),
+                               colour = "black", size = 2.9)
     }
     if(!is.null(xlim)) {
         out <- out + scale_x_continuous(limits = xlim)
