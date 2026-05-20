b0c932f1f1ac348c49c1da10ec0c8f5d0a03bae5
Péter Kalicz <kaliczp@gmail.com>
2026-05-20 07:00:42 +0200

    Ramp again

--- PlotFig6Rangsor.R
+++ PlotFig6Rangsor.R
@@ -8,5 +8,5 @@ dev.off()
 
 png("PlotFig6RangsorGradientKarcsú.png", height = 16, width =  5.8, pointsize = 13, units = "cm", res = 400)
 par(mar=c(5.1,3.1,0.1,0.1))
-makeRangsor(RangsorO, xlim = c(-14.8,32.8), SmallPlacement = -3, LastSmallPlacement = 11)
+makeRangsor(RangsorO, xlim = c(-14.8,32.8), SmallPlacement = -3, LastSmallPlacement = 11, rampcolor = TRUE)
 dev.off()
