ca54755e67494203db9d9162f226beeab1a4eba9
Péter Kalicz <kaliczp@gmail.com>
2026-05-29 20:01:02 +0200

    Correcting one var and remove whitelines

--- ImportNew.R
+++ ImportNew.R
@@ -6,19 +6,16 @@ library(readxl)
 T1SubsectRegion.Type <- unlist(read_excel("kalicz_peti_tablaabrak.xlsx", sheet = 1, col_names = FALSE, range = 'A2:A6'), use.names = FALSE)
 ## Remove unit
 T1SubsectRegion.Type <- unlist(strsplit(T1SubsectRegion.Type, ","))[c(TRUE, FALSE)]
-T1SubsectRegion.Type[4] <- "Gross prod. in agriculture"
+T1SubsectRegion.Type[4:5] <- c("Gross prod. in agriculture", "Area in harvested area")
 T1SubsectRegion.Region <- as.character(read_excel("kalicz_peti_tablaabrak.xlsx", sheet = 1, col_names = FALSE, range = 'B7:H7'))
 T1SubsectRegion.Product <- c("Cereals", "Oil crops", "Fruits", "Vegetables", "Roots and tubers")
-
 ## Cereals
 T1SubsectRegion <- read_excel("kalicz_peti_tablaabrak.xlsx", sheet = 1, col_names = FALSE, range = 'B2:H6')
-
 T1Cereals <- data.frame(Product = T1SubsectRegion.Product[1],
                         Region = rep(T1SubsectRegion.Region, each = 5),
                         Type = T1SubsectRegion.Type,
                         Diff = round(as.numeric(as.matrix(T1SubsectRegion)))
                         )
-
 ## Oil crops
 T1SubsectRegion <- read_excel("kalicz_peti_tablaabrak.xlsx", sheet = 1, col_names = FALSE, range = 'B10:H14')
 T1OilCrops <- data.frame(Product = T1SubsectRegion.Product[2],
@@ -26,7 +23,6 @@ T1OilCrops <- data.frame(Product = T1SubsectRegion.Product[2],
                          Type = T1SubsectRegion.Type,
                          Diff = round(as.numeric(as.matrix(T1SubsectRegion)))
                          )
-
 ## Fruits
 T1SubsectRegion <- read_excel("kalicz_peti_tablaabrak.xlsx", sheet = 1, col_names = FALSE, range = 'B18:H22')
 T1Fruits <- data.frame(Product = T1SubsectRegion.Product[3],
@@ -34,7 +30,6 @@ T1Fruits <- data.frame(Product = T1SubsectRegion.Product[3],
                              Type = T1SubsectRegion.Type,
                              Diff = round(as.numeric(as.matrix(T1SubsectRegion)))
                              )
-
 ## Vegetables
 T1SubsectRegion <- read_excel("kalicz_peti_tablaabrak.xlsx", sheet = 1, col_names = FALSE, range = 'B26:H30')
 T1vegetables <- data.frame(Product = T1SubsectRegion.Product[4],
@@ -42,7 +37,6 @@ T1vegetables <- data.frame(Product = T1SubsectRegion.Product[4],
                            Type = T1SubsectRegion.Type,
                            Diff = round(as.numeric(as.matrix(T1SubsectRegion)))
                            )
-
 ## Roots and tubers
 T1SubsectRegion <- read_excel("kalicz_peti_tablaabrak.xlsx", sheet = 1, col_names = FALSE, range = 'B34:H38')
 T1roots <- data.frame(Product = T1SubsectRegion.Product[5],
@@ -51,12 +45,10 @@ T1roots <- data.frame(Product = T1SubsectRegion.Product[5],
                       Diff = round(as.numeric(as.matrix(T1SubsectRegion)))
                       )
 T1.df <- rbind(T1Cereals, T1OilCrops, T1Fruits, T1vegetables, T1roots)
-
-
+## Factor
 T1.df$Product <- factor(T1.df$Product, levels = T1SubsectRegion.Product)
 T1.df$Region <- factor(T1.df$Region, levels = T1SubsectRegion.Region)
 T1.df$Type <- factor(T1.df$Type, levels = T1SubsectRegion.Type[5:1])
-
 ## Unify spread in all Type
 OrderStandard <- order(T1.df[, "Type"])
 MakeStandard.df <- T1.df[OrderStandard, c("Type","Diff")]
