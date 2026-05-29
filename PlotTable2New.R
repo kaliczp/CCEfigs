12e2d05971b01fa0417fa676ec811b077557316e
Péter Kalicz <kaliczp@gmail.com>
2026-05-29 20:01:44 +0200

    Table 2 figure widht, height modifications

--- PlotTable2New.R
+++ PlotTable2New.R
@@ -33,6 +33,6 @@ outtext <- out +
     geom_text(aes(label = round(Diff, 1)),
               size = 3
 )
-pdf("PlotTable2NewStd.pdf")
+pdf("PlotTable2NewStd.pdf", width = 8, height = 6)
 outtext
 dev.off()
